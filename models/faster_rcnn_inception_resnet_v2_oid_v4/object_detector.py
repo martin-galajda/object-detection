ed375d363d6d14e6ddab86cddc6f442ce04a0c0c
Martin Galajda <433487@mail.muni.cz>
2019-09-06 16:17:18 +0200

    add support for detecting objects on loaded images

--- models/faster_rcnn_inception_resnet_v2_oid_v4/object_detector.py
+++ models/faster_rcnn_inception_resnet_v2_oid_v4/object_detector.py
@@ -15,8 +15,8 @@ class ObjectDetector:
             if path_to_classes is None \
             else build_class_index(path_to_classes)
 
-    def infer_object_detections(self, target_file_path: str):
-        orig_img_pil, img_np = load_pil_image_from_file(target_file_path)
+    def infer_object_detections_on_loaded_image(self, img_np: np.array):
+        orig_img_width, orig_img_height = img_np.shape[:2]
         img_np = np.expand_dims(img_np, 0)
         output_dict = infer_objects_in_image(image=img_np, inference_graph=self.inference_graph)
 
@@ -33,7 +33,7 @@ class ObjectDetector:
             detected_box = output_dict['detection_boxes'][i]
             top, left, bottom, right = detected_box
 
-            img_width, img_height = orig_img_pil.width, orig_img_pil.height
+            img_width, img_height = orig_img_width, orig_img_height
 
             detected_boxes.append([
                 left * img_width,  # x1
@@ -45,4 +45,8 @@ class ObjectDetector:
         detected_boxes = np.array(detected_boxes)
 
         return detected_boxes, detected_classes, detected_scores
+
+    def infer_object_detections(self, target_file_path: str):
+        orig_img_pil, img_np = load_pil_image_from_file(target_file_path)
+        return self.infer_object_detections_on_loaded_image(img_np)
 
--- models/yolov3/object_detector.py
+++ models/yolov3/object_detector.py
@@ -10,22 +10,26 @@ class ObjectDetector:
     name = 'YOLOv3'
 
     def __init__(self, *,
-                 detection_prob_treshold=0.25,
+        detection_prob_treshold=0.25,
                  path_to_model: str = None,
                  path_to_classes: str = None):
         self.model = restore_model() if path_to_model is None else restore_model(path_to_model)
         self.class_index_to_human_readable_class = load_classes() if path_to_classes is None else load_classes(path_to_classes)
         self.detection_prob_treshold = detection_prob_treshold
 
-    def infer_object_detections(self, target_file_path: str):
-        orig_img_pil, _ = load_pil_image_from_file(target_file_path)
-        img_np = resize_and_letter_box(np.array(orig_img_pil)/255., target_width = 608, target_height = 608)
+    def infer_object_detections_on_loaded_image(
+        self,
+        image_np: np.array,
+    ):
+        orig_img_width, orig_img_height = image_np.shape[:2]
+
+        img_np = resize_and_letter_box(image_np/255., target_width = 608, target_height = 608)
         img_np = np.expand_dims(img_np, 0)
         detected_boxes, detected_classes, detected_scores = infer_objects_in_image(
             image=img_np*255.,
             model=self.model,
-            orig_image_height=orig_img_pil.height,
-            orig_image_width=orig_img_pil.width,
+            orig_image_height=orig_img_height,
+            orig_image_width=orig_img_width,
             detection_prob_treshold = self.detection_prob_treshold)
 
         human_readable_classes = []
@@ -36,4 +40,9 @@ class ObjectDetector:
             human_readable_classes.append(curr_classes)
 
         return detected_boxes, human_readable_classes, detected_scores
+
+    def infer_object_detections(self, target_file_path: str):
+        orig_img_pil, _ = load_pil_image_from_file(target_file_path)
+        return self.infer_object_detections_on_loaded_image(np.array(orig_img_pil))
+
 
