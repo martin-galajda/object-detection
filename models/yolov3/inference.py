fix: incorrect dimensions when loading image for YOLOv3

models/yolov3/inference.py
@@ -1,6 +1,6 @@
 from models.yolov3.constants import PathConstants
 from keras.models import load_model
-from utils.non_max_suppression import non_max_suppression_fast, non_max_suppression
+from utils.non_max_suppression import non_max_suppression
 from utils.math import sigmoid
 import time
 
@@ -64,10 +64,10 @@ def get_corrected_boxes(*, box_width, box_height, box_x, box_y, orig_image_shape
 
     if float(model_w / orig_image_w) < float(model_h / orig_image_h):
         new_w = model_w
-        new_h = (orig_image_h * model_w) / orig_image_w
+        new_h = float(orig_image_h * model_w) / orig_image_w
     else:
         new_h = model_h
-        new_w = (orig_image_w * model_h) / orig_image_h
+        new_w = float(orig_image_w * model_h) / orig_image_h
 
     box_x = (box_x - (((model_w - new_w)/2.0)/model_w)) / float(new_w/model_w)
     box_y = (box_y - (((model_h - new_h)/2.0)/model_h)) / float(new_h/model_h)
@@ -126,12 +126,12 @@ def _detect_objects(*,
                     grid_cell_height = (np.exp(height_feat) * ANCHORS[anchor_start_idx][anchor_idx][1]) / MODEL_HEIGHT
 
                     box_left_x, box_left_y, box_right_x, box_right_y = get_corrected_boxes(
-                        box_width = grid_cell_width,
-                        box_height = grid_cell_height,
-                        box_x = box_center_x,
-                        box_y = box_center_y,
-                        orig_image_shape = (orig_image_width, orig_image_height),
-                        model_image_shape = (MODEL_WIDTH, MODEL_HEIGHT))
+                        box_width=grid_cell_width,
+                        box_height=grid_cell_height,
+                        box_x=box_center_x,
+                        box_y=box_center_y,
+                        orig_image_shape=(orig_image_width, orig_image_height),
+                        model_image_shape=(MODEL_WIDTH, MODEL_HEIGHT))
 
                     box_candidates += [[
                         box_left_x,

models/yolov3/object_detector.py
@@ -10,7 +10,7 @@ class ObjectDetector:
     name = 'YOLOv3'
 
     def __init__(self, *,
-        detection_prob_treshold=0.25,
+                 detection_prob_treshold=0.5,
                  path_to_model: str = None,
                  path_to_classes: str = None):
         self.model = restore_model() if path_to_model is None else restore_model(path_to_model)
@@ -21,12 +21,11 @@ class ObjectDetector:
         self,
         image_np: np.array,
     ):
-        orig_img_width, orig_img_height = image_np.shape[:2]
-
-        img_np = resize_and_letter_box(image_np/255., target_width = 608, target_height = 608)
+        orig_img_height, orig_img_width = image_np.shape[:2]
+        img_np = resize_and_letter_box(image_np / 255., target_width = 608, target_height = 608)
         img_np = np.expand_dims(img_np, 0)
         detected_boxes, detected_classes, detected_scores = infer_objects_in_image(
-            image=img_np*255.,
+            image =img_np * 255.,
             model=self.model,
             orig_image_height=orig_img_height,
             orig_image_width=orig_img_width,
@@ -44,5 +43,3 @@ class ObjectDetector:
     def infer_object_detections(self, target_file_path: str):
         orig_img_pil, _ = load_pil_image_from_file(target_file_path)
         return self.infer_object_detections_on_loaded_image(np.array(orig_img_pil))
-
-

utils/preprocess_image.py
@@ -24,7 +24,7 @@ def resize_and_letter_box(image, *, target_width: int, target_height: int):
     width_ratio = target_width / float(im_width)
     ratio = min(height_ratio, width_ratio)
 
-    image_resized = cv2.resize(image, dsize=(0,0), fx=ratio, fy=ratio, interpolation=cv2.INTER_LINEAR)
+    image_resized = cv2.resize(image, dsize=(0, 0), fx=ratio, fy=ratio, interpolation=cv2.INTER_LINEAR)
 
     letter_box = np.full((int(target_width), int(target_height), 3), 0.5)
     row_start = int((letter_box.shape[0] - image_resized.shape[0]) / 2)
@@ -58,6 +58,7 @@ def resize_and_letter_box_pil_image(image: PIL.Image, *, target_width: int, targ
 
     return letter_box
 
+
 def resize_and_letter_box_pil_image_2(image: PIL.Image, *, target_width: int, target_height: int):
     """
     Letter box (black bars) a color image (think pan & scan movie shown
@@ -69,13 +70,13 @@ def resize_and_letter_box_pil_image_2(image: PIL.Image, *, target_width: int, ta
     """
     iw, ih = image.size
     w, h = target_width, target_height
-    scale = min(w/iw, h/ih)
-    nw = int(iw*scale)
-    nh = int(ih*scale)
+    scale = min(w / iw, h / ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
 
-    image = image.resize((nw,nh), PIL.Image.BICUBIC)
-    new_image = PIL.Image.new('RGB', (w,h), (128,128,128))
-    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    image = image.resize((nw, nh), PIL.Image.BICUBIC)
+    new_image = PIL.Image.new('RGB', (w, h), (128, 128, 128))
+    new_image.paste(image, ((w - nw) // 2, (h - nh) // 2))
     return new_image
 
 