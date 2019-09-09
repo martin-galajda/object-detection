11ae63fcefec01b53a95137af6c5137dd5ccaa4f
Martin Galajda <433487@mail.muni.cz>
2019-09-09 11:52:05 +0200

    feat: Add YOLOv3 with GPU head

--- models/utils/visualize.py
+++ models/utils/visualize.py
@@ -16,6 +16,19 @@ def draw_detected_boxes_on_pil_image(detected_boxes: np.array, detected_classes:
         draw.text(((max(x1, 0)), (y1) - 10), ','.join(classes_with_scores), fill="red")
 
 
+def draw_detected_boxes_on_pil_image_v2(detected_boxes: np.array, detected_classes: list, detected_scores: np.array, img: PIL.Image):
+    draw = ImageDraw.Draw(img)
+    for idx, box in enumerate(detected_boxes):
+        x1, y1, x2, y2 = box
+        curr_detected_class_names = detected_classes[idx]
+        curr_detected_scores = detected_scores[idx]
+
+        classes_with_scores = f'{curr_detected_class_names} {int(curr_detected_scores * 100)}%'
+        draw_rectangle(draw, [x1, y1, x2, y2])
+        print(curr_detected_class_names)
+        draw.text(((max(x1, 0)), (y1) - 10), classes_with_scores, fill="red")
+
+
 def draw_rectangle(draw_context: ImageDraw, box_coordinates: np.array, color="red", width=3):
     x1, y1, x2, y2 = box_coordinates
     offset = 1
---
+++ models/yolov3_gpu_head/conversion/utils.py
@@ -0,0 +1,44 @@
+import configparser
+import io
+from collections import defaultdict
+from models.yolov3_gpu_head.constants import PathConstants
+
+
+def load_classes(path_to_classes_file = PathConstants.YOLOV3_LABELS_FILE_PATH):
+    class_idx_to_class_name = {}
+    with open(path_to_classes_file, 'r') as f:
+        for idx, class_name in enumerate(f):
+            class_idx_to_class_name[idx] = str(class_name).strip('\n').lower()
+
+    return class_idx_to_class_name
+
+
+def unique_config_sections(config_file):
+    """Convert all config sections to have unique names.
+
+    Adds unique suffixes to config sections for compability with configparser.
+    """
+    section_counters = defaultdict(int)
+    output_stream = io.StringIO()
+    with open(config_file) as fin:
+        for line in fin:
+            if line.startswith('['):
+                section = line.strip().strip('[]')
+                _section = section + '_' + str(section_counters[section])
+                section_counters[section] += 1
+                line = line.replace(section, _section)
+            output_stream.write(line)
+    output_stream.seek(0)
+    return output_stream
+
+
+def parse_darknet_config(path_to_config_file):
+    unique_config_file = unique_config_sections(path_to_config_file)
+    cfg_parser = configparser.ConfigParser()
+    cfg_parser.read_file(unique_config_file)
+
+    for section in cfg_parser.sections():
+        print('Parsing section {}'.format(section))
+        print(dict(cfg_parser[section]))
+
+    return cfg_parser
