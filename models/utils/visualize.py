da0b988bff8f37e3e357421db5f9e09ca0931acf
Martin Galajda <433487@mail.muni.cz>
2019-09-15 15:08:52 +0200

    feat: drawing logic for bounding boxes

--- models/utils/visualize.py
+++ models/utils/visualize.py
@@ -1,6 +1,8 @@
 import numpy as np
 import PIL
 from PIL import ImageDraw
+from typing import List
+from models.data.bounding_box import BoundingBox
 
 
 def draw_detected_boxes_on_pil_image(detected_boxes: np.array, detected_classes: list, detected_scores: np.array, img: PIL.Image):
@@ -27,6 +29,14 @@ def draw_detected_boxes_on_pil_image_v2(detected_boxes: np.array, detected_class
         draw_rectangle(draw, [x1, y1, x2, y2])
         print(curr_detected_class_names)
         draw.text(((max(x1, 0)), (y1) - 10), classes_with_scores, fill="red")
+
+
+def draw_bounding_boxes_on_pil_image(bounding_boxes: List[BoundingBox], img: PIL.Image):
+    draw = ImageDraw.Draw(img)
+    for box in bounding_boxes:
+        classes_with_scores = f'{box.human_readable_class} {int(box.score * 100)}%'
+        draw_rectangle(draw, [box.min_x, box.min_y, box.max_x, box.max_y])
+        draw.text(((max(box.min_x, 0)), (box.min_y) - 10), classes_with_scores, fill="red")
 
 
 def draw_rectangle(draw_context: ImageDraw, box_coordinates: np.array, color="red", width=3):
