d1bbe4d1b095ae63b2284dddaf55ce9331a9b96a
Martin Galajda <433487@mail.muni.cz>
2019-12-03 07:14:14 -0800

    chore: clean up

--- web/index.ts
+++
@@ -1,6 +0,0 @@
-import { api, Api } from './api'
-import { log } from './logger'
-
-api.start().then(() => {
-  log.info('Api successfully started')
-}).catch(Api.fatal)
--- web/logger.ts
+++
@@ -1,7 +0,0 @@
-import * as pino from 'pino'
-import { config } from './config'
-
-export const log = pino({
-  ...config.logging,
-  serializers: pino.stdSerializers,
-})
